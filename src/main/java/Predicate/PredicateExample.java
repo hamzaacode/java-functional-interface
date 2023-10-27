46b62e284de48192b7a1995699d9ef36cab455f4
hamzaacode <amir.hamza.code@gmail.com>
2023-10-27 17:15:01 +0600

    Filter only string from a list

--- src/main/java/Predicate/PredicateExample.java
+++ src/main/java/Predicate/PredicateExample.java
@@ -22,6 +22,28 @@ public class PredicateExample {
 
         // Print the filtered list
         System.out.println("Even numbers: " + evenNumbers);
+
+
+        // Create a list of integers and String
+        List list = new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(20);
+        list.add(25);
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        // Define a Predicate to check String
+        Predicate<String> pureString=str->str.matches("^[a-zA-Z]*$");
+
+      List<String> listOfStrings= filterString(list,pureString);
+
+        // Print the filtered str
+        System.out.println("Strings: " + listOfStrings);
+
     }
 
     // Method to filter a list using a Predicate
@@ -33,5 +55,15 @@ public class PredicateExample {
             }
         }
         return filteredList;
+    }
+
+    public static List<String> filterString(List list, Predicate<String> pureString) {
+        List<String> filteredString = new ArrayList<>();
+        for (Object str : list) {
+            if (pureString.test(str.toString())) {
+                filteredString.add(str.toString());
+            }
+        }
+        return filteredString;
     }
 }
